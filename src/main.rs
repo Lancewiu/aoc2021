0b2e40c52cbf41b501a85aeaf3cd94762dd34133
Lawrence Niu <ranchwiu@gmail.com>
2022-09-21 22:21:39 -0400

    finishe day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,33 @@ use anyhow::Result;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn process_lines(mut reader: impl BufRead) -> Result<u32> {
-    let mut line = String::new();
+fn process_lines(reader: impl BufRead) -> Result<u32> {
     let mut prev_depth: u32 = 0;
     let mut depth_count: u32 = 0;
-    loop {
-        if 0 == reader.read_line(&mut line)? {
-            break;
-        }
+    for line_result in reader.lines() {
+        let line = line_result?;
         let depth: u32 = line.parse()?;
-
-        if 0 > prev_depth && depth > prev_depth {
+        if 0 < prev_depth && depth > prev_depth {
             depth_count += 1;
         }
         prev_depth = depth;
     }
-
     return Ok(depth_count);
 }
 fn main() {
-    const input_path: &str = "data/input.txt";
+    const INPUT_PATH: &str = "data/input.txt";
 
-    match File::open(input_path) {
+    match File::open(INPUT_PATH) {
         Ok(file) => match process_lines(BufReader::new(file)) {
             Err(err) => {
-                eprintln!("Could not process file {}:\n  {}", input_path, err);
+                eprintln!("Could not process file {}:\n  {}", INPUT_PATH, err);
             }
             Ok(depth_count) => {
                 println!("Depth Count: {}", depth_count);
             }
         },
         Err(err) => {
-            eprintln!("Error opening file {}:\n {}", input_path, err);
+            eprintln!("Error opening file {}:\n  {}", INPUT_PATH, err);
         }
     }
 }
