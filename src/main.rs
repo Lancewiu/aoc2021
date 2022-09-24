ea1b4e600940b6e4d1ffc0276ff4785c1ea88ec4
Lawrence Niu <ranchwiu@gmail.com>
2022-09-24 15:28:20 -0400

    complete day 6 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
-mod canvas;
-mod geometry;
-
-use anyhow::Result;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::str;
 
-fn process_lines(reader: impl BufRead) -> Result<u64> {
-    let mut board = canvas::Canvas::new();
-    for line_res in reader.lines() {
-        let line_str = line_res?;
-        let line: geometry::Line = line_str.parse()?;
-        board.count_line(&line);
+fn process_lines(mut reader: impl BufRead) -> anyhow::Result<usize> {
+    let mut line = String::new();
+    reader.read_to_string(&mut line)?;
+    let mut school: Vec<u32> = line
+        .trim()
+        .split(',')
+        .map(|token| token.trim().parse())
+        .collect::<Result<Vec<u32>, _>>()?;
+    for _ in 0..80 {
+        let mut num_new_fish = 0u32;
+        for fish in school.iter_mut() {
+            if 0 == *fish {
+                *fish = 6;
+                num_new_fish += 1;
+            } else {
+                *fish -= 1;
+            }
+        }
+        (0..num_new_fish).for_each(|_| school.push(8));
     }
-    Ok(board.iter().filter(|(_, counts)| 1 < **counts).count() as u64)
+    Ok(school.len())
 }
 fn main() {
     const INPUT_PATH: &str = "data/input.txt";
@@ -23,8 +32,8 @@ fn main() {
             Err(err) => {
                 eprintln!("Could not process file {}:\n  {}", INPUT_PATH, err);
             }
-            Ok(count) => {
-                println!("# overlapping points: {}", count);
+            Ok(num_fish) => {
+                println!("# fish: {}", num_fish);
             }
         },
         Err(err) => {
