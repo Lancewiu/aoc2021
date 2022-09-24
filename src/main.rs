e1ebd0612d73d2a301b6daabbf050d6cd7d406c9
Lawrence Niu <ranchwiu@gmail.com>
2022-09-24 15:46:57 -0400

    complete day 6 part 2

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,25 @@ use std::str;
 fn process_lines(mut reader: impl BufRead) -> anyhow::Result<usize> {
     let mut line = String::new();
     reader.read_to_string(&mut line)?;
-    let mut school: Vec<u32> = line
+    let mut histogram = [0usize; 9];
+    let initial_state = line
         .trim()
         .split(',')
-        .map(|token| token.trim().parse())
-        .collect::<Result<Vec<u32>, _>>()?;
-    for _ in 0..80 {
-        let mut num_new_fish = 0u32;
-        for fish in school.iter_mut() {
-            if 0 == *fish {
-                *fish = 6;
-                num_new_fish += 1;
-            } else {
-                *fish -= 1;
-            }
+        .map(|token| token.trim().parse::<usize>())
+        .collect::<Result<Vec<usize>, _>>()?;
+    initial_state.into_iter().for_each(|i| {
+        histogram[i] += 1;
+    });
+
+    for _ in 0..256 {
+        let num_reset_fish = histogram[0];
+        for i in 0usize..8 {
+            histogram[i] = histogram[i + 1];
         }
-        (0..num_new_fish).for_each(|_| school.push(8));
+        histogram[6] += num_reset_fish;
+        histogram[8] = num_reset_fish;
     }
-    Ok(school.len())
+    Ok(histogram.into_iter().sum())
 }
 fn main() {
     const INPUT_PATH: &str = "data/input.txt";
