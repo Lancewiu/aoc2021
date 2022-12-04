c9c385c776738ca35b2a72250a8c28f60f747912
Lawrence Niu <ranchwiu@gmail.com>
2022-12-04 17:07:43 -0500

    add debugging

--- src/main.rs
+++ src/main.rs
@@ -2,79 +2,99 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::str;
 
-const fn invert_bracket(bracket: char) -> Option<char> {
-    match bracket {
-        ')' => Some('('),
-        '(' => Some(')'),
-        '[' => Some(']'),
-        ']' => Some('['),
-        '{' => Some('}'),
-        '}' => Some('{'),
-        '<' => Some('>'),
-        '>' => Some('<'),
-        _ => None,
+fn debug_octopi(octopi: &[u32]) {
+    for row in 0..10 {
+        let start = row * 10;
+        let end = start + 10;
+        eprintln!("{}: {:?}", row, &octopi[start..end]);
     }
+    eprintln!();
 }
 
-const fn score_bracket(bracket: char) -> Option<u64> {
-    match bracket {
-        ')' => Some(1),
-        ']' => Some(2),
-        '}' => Some(3),
-        '>' => Some(4),
-        _ => None,
-    }
-}
-
-fn complete_token(line: &str) -> Option<Vec<char>> {
-    let mut prev_open_brackets: Vec<char> = Vec::new();
-    for token in line.chars() {
-        match token {
-            '(' | '[' | '{' | '<' => {
-                prev_open_brackets.push(token);
+fn empower_adjacent(octopi: &mut [u32], index: usize) {
+    let x = (index % 10) as i32;
+    let y = (index / 10) as i32;
+    for row in -1..2 {
+        for col in -1..2 {
+            let adj_x = x + col;
+            let adj_y = y + row;
+            let adjacent_i = adj_x + adj_y * 10;
+            if index as i32 == adjacent_i
+                || !(0..100).contains(&adjacent_i)
+                || adj_x > 9
+                || adj_y > 9
+            {
+                continue;
             }
-            ')' | ']' | '}' | '>' => {
-                let opposite = unsafe { invert_bracket(token).unwrap_unchecked() };
-                if Some(opposite) != prev_open_brackets.pop() {
-                    return None;
-                }
-            }
-            _ => unreachable!(),
+            octopi[adjacent_i as usize] += 1;
         }
     }
-    prev_open_brackets
-        .into_iter()
-        .rev()
-        .map(invert_bracket)
-        .collect::<Option<Vec<char>>>()
 }
 
-fn score_chunk(chunk: &[char]) -> Option<u64> {
-    let mut score = 0u64;
-    for token in chunk.iter().copied() {
-        score *= 5;
-        if let Some(score_value) = score_bracket(token) {
-            score += score_value;
-        } else {
-            return None;
+fn cascade_flashes(octopi: &mut [u32]) -> u64 {
+    let mut flashed: Vec<usize> = Vec::new();
+    let mut num_flashes: u64 = 0;
+
+    loop {
+        let new_flashes: Vec<usize> = octopi
+            .iter()
+            .enumerate()
+            .filter(|(_, octopus)| 10 <= **octopus)
+            .filter_map(|(i, _)| match flashed.binary_search(&i) {
+                Ok(_) => None,
+                Err(_) => Some(i),
+            })
+            .collect();
+        if new_flashes.is_empty() {
+            break;
+        }
+        num_flashes += new_flashes.len() as u64;
+        flashed.extend_from_slice(&new_flashes[..]);
+        flashed.sort();
+
+        for flash_index in new_flashes.into_iter() {
+            empower_adjacent(octopi, flash_index);
         }
     }
 
-    Some(score)
+    num_flashes
 }
 
 fn process_lines(reader: impl BufRead) -> anyhow::Result<u64> {
-    let mut scores: Vec<u64> = Vec::new();
+    let mut octopi: Vec<u32> = Vec::new();
     for line_result in reader.lines() {
-        if let Some(complete_chunk) = complete_token(line_result?.as_str()) {
-            scores.push(score_chunk(&complete_chunk[..]).expect("invalid chunk given"));
-        }
+        let line = line_result?;
+        let mut powers: Vec<u32> = line
+            .chars()
+            .map(|digit| digit.to_digit(10))
+            .collect::<Option<Vec<u32>>>()
+            .ok_or_else(|| anyhow::Error::msg("invalid digit encountered"))?;
+        octopi.append(&mut powers);
     }
-    scores.sort();
-    Ok(scores[scores.len() / 2])
+
+    let mut num_flashes: u64 = 0;
+    for step in 0..10 {
+        eprintln!("Step {}", step);
+        debug_octopi(&octopi[..]);
+        octopi.iter_mut().for_each(|octopus| {
+            *octopus += 1;
+        });
+
+        num_flashes += cascade_flashes(&mut octopi[..]);
+
+        octopi
+            .iter_mut()
+            .filter(|octopus| 10 <= **octopus)
+            .for_each(|flashed| {
+                *flashed = 0;
+            });
+    }
+
+    Ok(num_flashes)
 }
 fn main() {
-    const INPUT_PATH: &str = "data/input.txt";
+    //const INPUT_PATH: &str = "data/input.txt";
+    const INPUT_PATH: &str = "data/test_input.txt";
 
     match File::open(INPUT_PATH) {
         Ok(file) => match process_lines(BufReader::new(file)) {
